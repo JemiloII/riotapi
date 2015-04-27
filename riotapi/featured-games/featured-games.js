71259c2c8f8a52139ce680eca3b2750ec72826dc
jemiloii <jemiloii@jemiloii.com>
2015-04-26 21:33:28 -0500

    Featured-games working, just needs tests to prove it.

--- riotapi/featured-games/featured-games.js
+++ riotapi/featured-games/featured-games.js
@@ -1,15 +1,18 @@
 'use strict';
 
-var keysInValues = require('../../helpers/keysInValues'),
-	_            = require('../../helpers/lambda-ramda'),
+var keysInValues = require('../../lib/helpers/keysInValues'),
+	_            = require('../../lib/helpers/lambda-ramda'),
 	config       = require('config'),
+	log			 = require('../../lib/logger').getLogger(),
 	request      = require('sync-request'),
 
 	featuredGames = function (region, options) {
 		var encoding = options && options.encoding || config.encoding,
-			region = region && region.constructor === String ? region : options.region || config.region,
-			url = 'https://' + region + '.api.pvp.net/observer-mode/featured?api_key=' + config.api_key;
+			region = region && region.constructor === String ? region : options && options.region || config.region,
+			url = 'https://' + region + '.api.pvp.net/observer-mode/rest/featured?api_key=' + config.api_key;
+		log.info(url);
 		return request('GET', url).getBody(encoding);
 	}
 
 module.exports = featuredGames;
+
